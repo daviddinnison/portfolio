094447ff1474e640052c2b2c70f7612fdf364b91
daviddinnison <david.g.dinnison@gmail.com>
2017-10-04 03:20:51 -0400

    commented out scroll follow in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -8,4 +8,16 @@ function eventHandling() {
 
 $(document).ready(function() {
     eventHandling();
+    // $(window).scroll(function () {
+    //     //if you hard code, then use console
+    //     //.log to determine when you want the 
+    //     //nav bar to stick.  
+    //     console.log($(window).scrollTop())
+    //   if ($(window).scrollTop() > 280) {
+    //     $('.main-nav').addClass('navbar-fixed');
+    //   }
+    //   if ($(window).scrollTop() < 281) {
+    //     $('.main-nav').removeClass('navbar-fixed');
+    //   }
+    // });
 });
