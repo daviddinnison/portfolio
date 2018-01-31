3450198471964aaff18462f58b6442a23d802595
daviddinnison <david.g.dinnison@gmail.com>
2018-01-31 02:58:59 -0500

    initial redesign

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,34 @@
 //smooth scrolling for nav ids on click
-function navIdScrolling() {
-    $('a[href*=\\#]').on('click', function (event) {
-        event.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 700);
-    });
-}
+// function navIdScrolling() {
+//     $('a[href*=\\#]').on('click', function (event) {
+//         event.preventDefault();
+//         $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 700);
+//     });
+// }
 
 //nav bar fixed on scrolling
-function fixedNavScrolling() {
-    const mainNav = $(".main-nav");
+// function fixedNavScrolling() {
+//     const mainTitle = $(".titles");
 
-    const scrolledNav = "main-nav-scrolled";
+//     const scrolledTitle = "main-title-scrolled";
 
-    const headerHeight = $('header').outerHeight();
-    const aboutNavScroll = "about-nav-scroll";
-    const about = $('.about');
+//     const titleHeight = $('title').outerHeight();
+//     const aboutTitleScroll = "about-title-scroll";
+//     const about = $('.about');
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > headerHeight) {
-            mainNav.addClass(scrolledNav);
-            about.addClass(aboutNavScroll);
-        } else {
-            mainNav.removeClass(scrolledNav);
-            about.removeClass(aboutNavScroll);
-        }
-    });
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() >titleHeight) {
+//             mainTitle.addClass(scrolledTitle);
+//             about.addClass(aboutTitleScroll);
+//         } else {
+//             mainTitle.removeClass(scrolledTitle);
+//             about.removeClass(aboutTitleScroll);
+//         }
+//     });
 }
 
 
 $(document).ready(function () {
-    navIdScrolling();
-    fixedNavScrolling();
+    // navIdScrolling();
+    // fixedNavScrolling();
 }); 
