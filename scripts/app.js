110eaeb8969bc6febc4e908a7eec579aa228f1c9
daviddinnison <david.g.dinnison@gmail.com>
2018-02-07 01:56:48 -0500

    brought back nav scrolling

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,11 @@
 //smooth scrolling for nav ids on click
-// function navIdScrolling() {
-//     $('a[href*=\\#]').on('click', function (event) {
-//         event.preventDefault();
-//         $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 700);
-//     });
-// }
-
-//nav bar fixed on scrolling
-// function fixedNavScrolling() {
-//     const mainTitle = $(".titles");
-
-//     const scrolledTitle = "main-title-scrolled";
-
-//     const titleHeight = $('title').outerHeight();
-//     const aboutTitleScroll = "about-title-scroll";
-//     const about = $('.about');
-
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() >titleHeight) {
-//             mainTitle.addClass(scrolledTitle);
-//             about.addClass(aboutTitleScroll);
-//         } else {
-//             mainTitle.removeClass(scrolledTitle);
-//             about.removeClass(aboutTitleScroll);
-//         }
-//     });
+const navIdScrolling = () => {
+    $('a[href*=\\#]').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 700);
+    });
 }
 
-
 $(document).ready(function () {
-    // navIdScrolling();
-    // fixedNavScrolling();
+    navIdScrolling();
 }); 
